d70fa0979333f262997772ae1d21babf694e8f54
Valerie-0036 <valerie.535210036@stu.untar.ac.id>
2022-09-10 21:52:24 +0700

    responsivity over design

--- js/app.js
+++ js/app.js
@@ -7,3 +7,28 @@ const mobileMenu = () => {
     menuLinks.classList.toggle('active')
 }
 menu.addEventListener('click', mobileMenu)
+
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
